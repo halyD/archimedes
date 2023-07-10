47674aa0341a22d3e93ff9712210a9e24c8dbbda
Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
2023-07-10 06:30:47 -0700

    kernel for depthwise convolution

---
+++ archimedes_tests/DW3x3/inc/neureka_scale.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Mantainer: Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
+ */
+#define STIM_NQ_SIZE 64
+// scale [32]
+int8_t  L1_DATA neureka_scale[] = {
+  0x30,
+  0x08,
+  0x04,
+  0x36,
+  0x30,
+  0x08,
+  0x04,
+  0x36,
+  0x0f,
+  0x1e,
+  0x0a,
+  0x0d,
+  0x09,
+  0x2f,
+  0x02,
+  0x08,
+  0x36,
+  0x35,
+  0x08,
+  0x36,
+  0x35,
+  0x0d,
+  0x1b,
+  0x22,
+  0x33,
+  0x29,
+  0x1c,
+  0x23,
+  0x1e,
+  0x36,
+  0x05,
+  0x1a,
+  0x3d,
+  0x25,
+  0x32,
+  0x0a
+};
+
