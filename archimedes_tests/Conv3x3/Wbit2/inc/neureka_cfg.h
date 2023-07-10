0566ca4561aa1662a05c52b2edc2c80d9c0c0254
Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
2023-07-10 06:31:17 -0700

    kernel for 3x3 convolution (Wbit2 for Activation=8bits and weight=2bits)

---
+++ archimedes_tests/Conv3x3/Wbit2/inc/neureka_cfg.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2018 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Mantainer: Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
+ */
+int32_t neureka_cfg[] = {
+  0xffffffff,
+  0xffffffff,
+  0xffffffff,
+  0xffffffff,
+  0xffffffff,
+  0xffffffff,
+  0xffffffff,
+  0x00000020,
+  0x00000100,
+  0x00000800,
+  0x00000020,
+  0x00000020,
+  0x000000c0,
+  0x00000024,
+  0x00000048,
+  0x00000000,
+  0x00000020,
+  0x00000000,
+  0x00000000,
+  0x00010001,
+  0x00010001,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x03850811
+};
+
---
+++ archimedes_tests/Conv3x3/Wbit2/inc/neureka_scale_bias.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Mantainer: Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
+ */
+#define STIM_NQB_SIZE 256
+// scale_bias [32]
+int32_t  L1_DATA neureka_scale_bias[] = {
+  0x01e34add,
+  0xfcc05695,
+  0x0dd21738,
+  0xf5ed64cb,
+  0xf061ecbc,
+  0x0cf716d2,
+  0xff2835e3,
+  0x00635cc2,
+  0x0f3decac,
+  0x05ee7dae,
+  0x0d7a43a5,
+  0x04211df5,
+  0xf503d9f8,
+  0xfa576093,
+  0xf71d6bce,
+  0xfdd5319f,
+  0x0ca15bd2,
+  0x09d21735,
+  0xfa763556,
+  0x0e91e680,
+  0xff61b5d4,
+  0x02b98c40,
+  0x0c022240,
+  0x07077100,
+  0xfab1d5a8,
+  0x076b85b7,
+  0xf4267bab,
+  0x01d02c4a,
+  0x0950792b,
+  0x00b3adf4,
+  0x09e80602,
+  0x030482f3
+};
+
