adbcc8f30586006a627771d248196a120799f093
Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
2023-07-10 06:29:48 -0700

    kernel for pointwise convolution

---
+++ archimedes_tests/Conv1x1/inc/neureka_scale.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Mantainer: Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
+ */
+#define STIM_NQ_SIZE 64
+// scale [32]
+int8_t  L1_DATA neureka_scale[] = {
+  0x18,
+  0x1b,
+  0x1f,
+  0x14,
+  0x18,
+  0x1b,
+  0x1f,
+  0x14,
+  0x09,
+  0x24,
+  0x36,
+  0x03,
+  0x29,
+  0x3d,
+  0x30,
+  0x3d,
+  0x2f,
+  0x12,
+  0x2a,
+  0x29,
+  0x2d,
+  0x31,
+  0x16,
+  0x3b,
+  0x2d,
+  0x2a,
+  0x2a,
+  0x1c,
+  0x30,
+  0x35,
+  0x2b,
+  0x33,
+  0x02,
+  0x3a,
+  0x05,
+  0x2c
+};
+
